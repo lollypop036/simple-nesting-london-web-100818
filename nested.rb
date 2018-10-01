ad2b3725525ad0b2f3d3612a6fb61f50342cce9f
Lola Onanuga <slonanuga@gmail.com>
2018-10-01 14:53:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +38,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-end
+     
+     programmer_hash[:alan_kay][:known_for]
+     
+     end
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +59,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  lang = programmer_hash[:dennis_ritchie][:languages].each {|x| x}
+  lang.join(",")
+   
 end
 
 def adding_matz
@@ -63,6 +71,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
+
 
 	programmer_hash = 
  		{
@@ -80,7 +90,8 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "c"]}
+    programmer_hash
 end
 
 def changing_alan
@@ -101,7 +112,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
+     programmer_hash
      
 end
 
@@ -121,5 +134,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
+programmer_hash
 end
