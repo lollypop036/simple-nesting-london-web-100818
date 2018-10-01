00694285752d1f7b29ea97246a7af9a3ba2c2d70
Lola Onanuga <slonanuga@gmail.com>
2018-10-01 14:47:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -71,7 +71,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "c"]
+
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "c"]}
 
 	programmer_hash = 
  		{
