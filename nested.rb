c11db973385b6fb49023b21245aa00312e790089
Lola Onanuga <slonanuga@gmail.com>
2018-10-01 14:37:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].select {|x| puts x}
+     programmer_hash[:dennis_ritchie][:languages].each {|x| x.join}
    
     
 end
